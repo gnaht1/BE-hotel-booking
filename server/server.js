09b0b53e2ffc7c447ba0e8456b9f662a6db8489b
Gnaht <rexming2016@gmail.com>
2025-10-08 10:18:12 +0700

    Add Clerk webhook integration and user model

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ import express, { response } from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhook from "./controllers/ClerkWebhooks.js";
+
 
 connectDB();
 
 const app = express();
 app.use(cors()); // Enable CORS for all routes
+
+// Clerk middleware
+app.use(express.json());
+app.use(clerkMiddleware())
+
+// api to listen to clerk webhook
+app.use("/api/clerk", clerkWebhook);
 
 app.get('/', (req, res) => res.send("API is working"));   
 
