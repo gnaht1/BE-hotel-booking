d0720cfc2f18105b5f0088755e2698452dc2e7aa
Gnaht <rexming2016@gmail.com>
2025-10-13 23:38:08 +0700

    fix bug

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 export const registerHotel = async (req, res) =>{
     try {
         const {name, address, contact, city} = req.body;
-        const owner = req.user_id;
+        const owner = req.user._id;
 
         // Check if User Already Registered
         const hotel = await Hotel.findOne({owner})
