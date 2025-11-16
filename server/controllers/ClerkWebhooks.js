7fd76cec70fa296afd2e3cabc14620e6136cd6aa
Gnaht <rexming2016@gmail.com>
2025-11-16 22:21:26 +0700

    Refactor user data handling in Clerk webhook
    Moved userData object creation inside 'user.created' and 'user.updated' cases to improve code clarity and prevent unnecessary variable allocation when not needed.

--- server/controllers/ClerkWebhooks.js
+++ server/controllers/ClerkWebhooks.js
@@ -19,23 +19,36 @@ const clerkWebhook = async (req, res) => {
         // Get data from the request body
         const {data, type} = req.body;
 
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url,
-            role: "user",
-            recentSearchCities: [],
-        }; 
+        
 
         // Switch case for different types of events
         switch (type){
-            case "user.created":
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                    role: "user",
+                    recentSearchCities: [],
+                }; 
+
                 await User.create(userData);
                 break;
-            case "user.updated":
+            }
+            case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                    role: "user",
+                    recentSearchCities: [],
+                }; 
+            
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
+            }
             case "user.deleted":
                 await User.findByIdAndDelete(data.id);
                 break;
